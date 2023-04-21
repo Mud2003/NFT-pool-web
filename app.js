9340cd6f8dd34d20b4aa70d452cc9fea13dfbafd
sonicprv <>
2023-04-21 14:18:13 +0530

    Make responsive

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// when clicked the humburger
 function showUl(elementId, propertyName) {
     var element = document.getElementById(elementId); // Get the element by ID
     var currentValue = element.style[propertyName]; // Get the current value of the CSS property
@@ -6,6 +7,8 @@ function showUl(elementId, propertyName) {
     element.style[propertyName] = newValue; // Set the CSS property value to the new value
 }
 
+
+// Resize the screenWidth
 function changeCSSProperty(elementId, propertyName) {
     var element = document.getElementById(elementId); // Get the element by ID
     var screenWidth = window.innerWidth; // Get the screen width
@@ -13,17 +16,45 @@ function changeCSSProperty(elementId, propertyName) {
   
     element.style[propertyName] = newValue; // Set the CSS property value to the new value
 }
-
 window.addEventListener('resize', function() {
     changeCSSProperty('hb', 'display');
 });
 
 
 
+// Resize and change nav back-color
+function changeCSSProperty(elementId, propertyName) {
+    var element = document.getElementById(elementId); // Get the element by ID
+    var screenWidth = window.innerWidth; // Get the screen width
+    var newValue = screenWidth < 1245 ? 'transparent' ? 'rgba(27, 14, 45, 1)' : 'transparent': 'transparent'; // Set the new value based on the screen width
+  
+    element.style[propertyName] = newValue; // Set the CSS property value to the new value
+}
+window.addEventListener('resize', function() {
+    changeCSSProperty('nav', 'background-color');
+});
+
+
+
+// scroll the navbar
+const navbar = document.querySelector('.navbar');
+window.addEventListener('scroll', () => {
+    navbar.classList.add('scrolled');
+    if (window.scrollY > 0) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+
+
+
+
 
 // Screen sizes for responsive site
 
-// 1522px for hero
+
 // @media screen and (max-width: 1245px) {
 
 // }
